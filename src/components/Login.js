f3b0cc9ba41fe47ed59d218f57f21894c7cde8df
Gabriel Brolo <gabrolo13@gmail.com>
2018-06-07 11:28:34 -0600

    Quick UI fixes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Button, FormGroup, Label, Input } from 'reactstrap';
+import { Button, FormGroup, Label, Input, Container, Row, Col } from 'reactstrap';
+
+import '../css/layout.css';
 
 class Login extends Component {
     constructor() {
@@ -31,59 +33,94 @@ class Login extends Component {
     render() {
         return (
             <div>
-                <FormGroup>
-                    <Label for="username">Username</Label>
-                    <Input 
-                        type="text" 
-                        value={this.state.username}
-                        name="username"  
-                        id="username" 
-                        placeholder="username"
-                        onChange={this.updateInput('username')} />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="password">Password</Label>
-                    <Input 
-                        type="password" 
-                        value={this.state.password}
-                        name="password"  
-                        id="password" 
-                        placeholder="password"
-                        onChange={this.updateInput('password')} />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="name">Name</Label>
-                    <Input 
-                        type="text" 
-                        value={this.state.name}
-                        name="name"  
-                        id="name" 
-                        placeholder="name"
-                        onChange={this.updateInput('name')} />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="surname">Surname</Label>
-                    <Input 
-                        type="text" 
-                        value={this.state.surname}
-                        name="surname"  
-                        id="surname" 
-                        placeholder="surname"
-                        onChange={this.updateInput('surname')} />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="email">Email</Label>
-                    <Input 
-                        type="email" 
-                        value={this.state.email}
-                        name="email"  
-                        id="email" 
-                        placeholder="email"
-                        onChange={this.updateInput('email')} />
-                </FormGroup>
+                <Container className="layout-container">
+                    <Row>
+                        <Col xs="5" className="layout-col">
+                            <div className="layout-form-title">
+                                <h2>Log In</h2>
+                            </div>
+                            <hr />
+                            <FormGroup className="layout-form-group">
+                                <Label for="username_login">Username</Label>
+                                <Input 
+                                    type="text"                                     
+                                    name="username_login"  
+                                    id="username_login" 
+                                    placeholder="Username"
+                                    onChange={this.updateInput('username')} />
+                            </FormGroup>
+                            <FormGroup className="layout-form-group">
+                                <Label for="password_login">Password</Label>
+                                <Input 
+                                    type="password"                                     
+                                    name="password_login"  
+                                    id="password_login" 
+                                    placeholder="Password"
+                                    onChange={this.updateInput('password')} />
+                            </FormGroup>
+                            <Button className="layout-btn-generic" color="primary" onClick={this.login}>Log In</Button>
+                        </Col>
 
-                <Button color="primary" onClick={this.login}>Log In</Button>
-                <Button color="primary" onClick={this.signup}>Sign Up</Button>
+                        <Col xs="2"></Col>
+
+                        <Col xs="5" className="layout-col">
+                            <div className="layout-form-title">
+                                <h2>Sign Up</h2>
+                            </div>
+                            <hr />
+                            <FormGroup className="layout-form-group">
+                                <Label for="username">Username</Label>
+                                <Input 
+                                    type="text"                                     
+                                    name="username"  
+                                    id="username" 
+                                    placeholder="Username"
+                                    onChange={this.updateInput('username')} />
+                            </FormGroup>
+                            <FormGroup className="layout-form-group">
+                                <Label for="password">Password</Label>
+                                <Input 
+                                    type="password"                                     
+                                    name="password"  
+                                    id="password" 
+                                    placeholder="Password"
+                                    onChange={this.updateInput('password')} />
+                            </FormGroup>
+                            <FormGroup className="layout-form-group">
+                                <Label for="name">Name</Label>
+                                <Input 
+                                    type="text" 
+                                    value={this.state.name}
+                                    name="name"  
+                                    id="name" 
+                                    placeholder="Name"
+                                    onChange={this.updateInput('name')} />
+                            </FormGroup>
+                            <FormGroup className="layout-form-group">
+                                <Label for="surname">Surname</Label>
+                                <Input 
+                                    type="text" 
+                                    value={this.state.surname}
+                                    name="surname"  
+                                    id="surname" 
+                                    placeholder="Surname"
+                                    onChange={this.updateInput('surname')} />
+                            </FormGroup>
+                            <FormGroup className="layout-form-group">
+                                <Label for="email">Email</Label>
+                                <Input 
+                                    type="email" 
+                                    value={this.state.email}
+                                    name="email"  
+                                    id="email" 
+                                    placeholder="Email"
+                                    onChange={this.updateInput('email')} />
+                            </FormGroup>            
+                            <Button color="primary" className="layout-btn-generic"  onClick={this.signup}>Sign Up</Button>
+                        </Col>
+                    </Row>
+                </Container>
+                
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,43 @@
 import React from 'react';
+import ReactLoading from 'react-loading';
 import { render } from 'react-dom';
 import history from './history';
 import { Router, Switch, Route, Redirect } from 'react-router-dom';
+import { Container, Row, Col } from 'reactstrap';
 import { App, Home } from './components';
 import Auth from './Auth';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/layout.css';
 
 const auth = new Auth();
+
+const callbackDialog = (text) => {
+    return (
+        <Container>
+            <Row>
+                <Col xs="5">
+                </Col>
+                <Col className="layout-loading-spin" xs="1">
+                    <h5>{text}...</h5>
+                </Col>
+                <Col className="layout-loading-spin" xs="1">
+                    <ReactLoading type="spin" color="#181818" height={50} width={25} />
+                </Col>
+                <Col xs="5">
+                </Col>
+            </Row>            
+        </Container>
+    );
+};
 
 const callbackComponent = () => {
     if (auth.loggedIn) {
         setTimeout(() => history.replace('/'), 1500);
-        return <h4>loading...</h4>;
+        return callbackDialog("Loading");
     } else {
         setTimeout(() => history.replace('/'), 1500);
-        return <h4>logging you out...</h4>;
+        return callbackDialog("Signout");
         return <Redirect to={{ pathname: '/' }} />
     }
 };
